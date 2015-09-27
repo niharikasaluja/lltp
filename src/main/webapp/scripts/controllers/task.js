9d17f463cdc338bcdd0b86d41ae847973ad6cd0b
Sorabh Saluja <sorabhsd@goldengekko.com>
2015-09-27 13:42:43 +0530

    Add task Details

--- src/main/webapp/scripts/controllers/task.js
+++ src/main/webapp/scripts/controllers/task.js
@@ -1,10 +1,21 @@
 angular.module('webappApp')
-.controller('TaskCtrl', function ($http, $timeout, $scope) {
+.controller('TaskCtrl', function ($http, $timeout, $scope, $routeParams) {
 
  console.log("In task page");
   $.material.init();
   $http.get("users/all").success(function putDataInScope(data) {
       $scope.users = data;
+      
+  $http.get('task/getTaskDetails?taskId=' + $routeParams.taskId).success(function(data) {
+	  console.log(data);
+	  $scope.task = data;
+	  console.log("task details");
+	  $scope.startDate = moment($scope.task['startDate']).format('Do');
+	  $scope.startMonth = moment($scope.task['startDate']).format('MMMM');
+	  $scope.lastUpdated = moment($scope.task['startDate']).fromNow();
+  }).error(function() {
+	  
+  });
      
   }).error(function tryAgainLater() {
      
